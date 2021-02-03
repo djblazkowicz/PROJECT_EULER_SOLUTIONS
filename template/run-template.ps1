8c50091673e0e1b77f69a178ae07ac9727a70869
djblazkowicz <56305919+djblazkowicz@users.noreply.github.com>
2021-02-03 19:00:50 +0000

    updated run-template.ps1

--- template/run-template.ps1
+++ template/run-template.ps1
@@ -4,19 +4,19 @@ $test = Test-Path .\out.txt
 
 if (!$test) {} else {remove-item .\out.txt}
 
-$app
+#$app
 
+#define array of input arguments to run the .exe with
 $set = 99,999,9999
 
 foreach ($item in $set)
 {
 
     
-    [System.Collections.arraylist]$out = @()
+    #so this will run whatever.exe 99 then whatever.exe 999 and so on..
+    #and put the console output in the out.txt text file
 
     $run = "&" + ($app + " " + $item) 
-
-   
 
     $asd = Invoke-Expression $run | out-file .\out.txt -Append
 
