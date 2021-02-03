f88a1d4b3f3d65a91cee31fa8d4a72899a7b8868
djblazkowicz <56305919+djblazkowicz@users.noreply.github.com>
2021-02-03 12:59:04 +0000

    updated template

--- template/template.c
+++ template/template.c
@@ -2,28 +2,32 @@
 #include <time.h>
 #include ".\template.h"
 
-//main() is only used to run MyCode and run stopwatch. put code to run in MyCode() in template.h
 int main(int argc, char **argv)
 {
     clock_t t;
 
-    printf (" start : %d \n", ( int ) ( t= clock () ));
+    //printf (" start : %d \n", ( int ) ( t= clock () ));
 
-    int input = atoi(argv[1]); //convert argument to integer
+    t = clock();
+
+    int input = atoi(argv[1]);
+
+    //int input = 999;
 
     if (input > 0) 
     {
-            MyCode(input); //MyCode lives in template.h
+            MyCode(input);
     } else
     {
-        printf("Please enter a number bigger than 0!"); //this does not work
+        printf("Please enter a number bigger than 0!");
     }
 
-    printf (" stop : %d \n", (int ) (t = clock () -t));
+    //printf (" stop : %d \n", (int ) (t = clock () -t));
 
-    printf ("%f seconds\n", ( double ) t / CLOCKS_PER_SEC ) ;
-    
-    printf (" INPUT SIZE : %d\n", input);
+    t = clock() - t;
+
+    printf("MEASUREMENT:%d,", input);
+    printf ("%f\n", ( double ) t / CLOCKS_PER_SEC ) ;
 
     return 0;
 }
